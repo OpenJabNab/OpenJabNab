ab7c7fba8b15e1faa183e22765c46b0ad2362183
Alkorin <alkorin@openjabnab.org>
2008-02-12 14:03:19 +0000

    Ajout du service Taï Chi Ajout de quelques valeurs par défaut pour certains services
    git-svn-id: https://openjabnab.svn.sourceforge.net/svnroot/openjabnab@19 1b6914c7-4944-0410-9b9d-ee7decc8b50e

--- trunk/openjabnab/lib/ambientpacket.h
+++ trunk/openjabnab/lib/ambientpacket.h
@@ -10,8 +10,11 @@
 class OJN_EXPORT AmbientPacket : public Packet
 {
 public:
-	enum Services { Disable_Service = 0, Service_Weather, Service_Bourse, Service_Periph, MoveLeftEar, MoveRightEar, Service_Mail, Service_AirQuality, Service_Noze};
-	enum Weather_Values { Weather_Sun = 0, Weather_Cloudy, Weather_B, Weather_Rain, Weather_Snow, Weather_O};
+	enum Services { Disable_Service = 0, Service_Weather, Service_StockMarket, Service_Periph, MoveLeftEar, MoveRightEar, Service_EMail, Service_AirQuality, Service_Noze, Service_TaiChi = 0x0e};
+	enum Weather_Values { Weather_Sun = 0, Weather_Cloudy, Weather_Smog, Weather_Rain, Weather_Snow, Weather_Storm};
+	enum StockMarket_Values { StockMarket_HighDown = 0, StockMarket_MediumDown, StockMarket_LittleDown, StockMarket_Stable, StockMarket_LittleUp, StockMarket_MediumUp, StockMarket_HighUp};
+	enum EMail_Values { EMail_No = 0, EMail_1, EMail_2, EMail_3AndMore };
+	enum Noze_Values { Noze_No = 0, Noze_Blink, Noze_DoubleBlink };
 
 	AmbientPacket() {};
 	AmbientPacket(enum Services, unsigned char value);
