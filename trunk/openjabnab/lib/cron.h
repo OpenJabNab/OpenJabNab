e2e57ff88f46ae7eb8bb6c677a6a991de873a64a
Alkorin <alkorin@openjabnab.org>
2008-09-09 13:23:03 +0000

    Modified Cron, now it's possible to pass a QVariant to Cron::Register instead of an unsigned int and Bunny* is now a QVariant type Modified Cron to add a string as 6th arg which is the name of the func who will be called by the cron.
    git-svn-id: https://openjabnab.svn.sourceforge.net/svnroot/openjabnab@45 1b6914c7-4944-0410-9b9d-ee7decc8b50e

--- trunk/openjabnab/lib/cron.h
+++ trunk/openjabnab/lib/cron.h
@@ -4,13 +4,15 @@
 #include "global.h"
 #include <QObject>
 #include <QVector>
+#include <QVariant>
 
 class PluginInterface;
 
 typedef struct {
-	PluginInterface * plugin;
+	QObject * plugin;
+	QVariant data;
+	const char * callback;
 	unsigned int interval; // in min
-	unsigned int id;
 	unsigned int next_run; // in seconds since 1970-01-01T00:00:00
 } CronElement;
 
@@ -19,9 +21,9 @@ class OJN_EXPORT Cron : public QObject
 	Q_OBJECT
 	
 public:
-	static bool Register(PluginInterface *, unsigned int interval, unsigned int offsetH, unsigned int offsetM, unsigned int id = 0);
-	static void Unregister(PluginInterface *, unsigned int id);
-	static void UnregisterAll(PluginInterface *);
+	static bool Register(QObject *, unsigned int interval, unsigned int offsetH, unsigned int offsetM, QVariant data = QVariant(), const char * callback = 0);
+	static void Unregister(QObject *, QVariant data);
+	static void UnregisterAll(QObject *);
 
 private slots:
 	void OnTimer();
