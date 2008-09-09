980cb94d7e5d3d007bba1c5e3ca68fddb994b383
Alkorin <alkorin@openjabnab.org>
2008-09-09 15:47:25 +0000

    Cron::Register returns now an unsigned int which can be used with Unregister to cancel this one
    git-svn-id: https://openjabnab.svn.sourceforge.net/svnroot/openjabnab@48 1b6914c7-4944-0410-9b9d-ee7decc8b50e

--- trunk/openjabnab/lib/cron.h
+++ trunk/openjabnab/lib/cron.h
@@ -3,8 +3,8 @@
 
 #include "global.h"
 #include <QObject>
-#include <QVector>
 #include <QVariant>
+#include <QList>
 
 class PluginInterface;
 
@@ -12,6 +12,7 @@ typedef struct {
 	QObject * plugin;
 	QVariant data;
 	const char * callback;
+	unsigned int id;
 	unsigned int interval; // in min
 	unsigned int next_run; // in seconds since 1970-01-01T00:00:00
 } CronElement;
@@ -21,9 +22,10 @@ class OJN_EXPORT Cron : public QObject
 	Q_OBJECT
 	
 public:
-	static bool Register(QObject *, unsigned int interval, unsigned int offsetH, unsigned int offsetM, QVariant data = QVariant(), const char * callback = 0);
-	static void Unregister(QObject *, QVariant data);
+	static unsigned int Register(QObject *, unsigned int interval, unsigned int offsetH, unsigned int offsetM, QVariant data = QVariant(), const char * callback = 0);
+	static void Unregister(QObject *, unsigned int id);
 	static void UnregisterAll(QObject *);
+	static QList<CronElement> GetByData(QObject *, QVariant);
 
 private slots:
 	void OnTimer();
@@ -31,7 +33,8 @@ private slots:
 private:
 	Cron();
 	static Cron& Instance();
-	QVector<CronElement> CronElements;
+	unsigned int lastGivenID;
+	QList<CronElement> CronElements;
 };
 
 #endif
