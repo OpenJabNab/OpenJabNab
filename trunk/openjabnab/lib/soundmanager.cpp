8f9ef3930058d741bc2e8e8b322e8aa4b0972ce5
Alkorin <alkorin@openjabnab.org>
2008-09-15 07:46:12 +0000

    Added SoundManager to lib.pro Moved PluginInterface settings to protected Fixed a memory leak in SoundManager
    git-svn-id: https://openjabnab.svn.sourceforge.net/svnroot/openjabnab@55 1b6914c7-4944-0410-9b9d-ee7decc8b50e

--- trunk/openjabnab/lib/soundmanager.cpp
+++ trunk/openjabnab/lib/soundmanager.cpp
@@ -2,6 +2,8 @@
 #include <QStringList>
 #include <QUrl>
 #include <QFile>
+#include <QHttpRequestHeader>
+#include <QDataStream>
 #include "log.h"
 #include "soundmanager.h"
 #include "settings.h"
@@ -15,28 +17,27 @@ bool SoundManager::createNewSound(QString text, QString voice, QString fileName)
 	if(!voiceList.contains(voice))
 		voice = "claire";
 
-	QString HeaderData = "";
-	QByteArray ContentData = "";
+	QHttp http("vaas3.acapela-group.com");
+	connect(&http, SIGNAL(done(bool)), this, SLOT(downloadDone()));
 
+	QByteArray ContentData;
 	ContentData += "php%5Fvar%5Fdec=undefined&php%5Fvar%5Fhtml=undefined&php%5Fvar%5Fstring=" + voice + "22k%5B%2Fvoix%5D" + QUrl::toPercentEncoding(text) + "&php%5Fvar%5Fnom=whatever%2Etxt&php%5Fverif=flash&onLoad=%5Btype%20Function%5D";
 
-	HeaderData += "POST /Demo_Web/write.php HTTP/1.1\r\n";
-	HeaderData += "Host:vaas3.acapela-group.com\r\n";
-	HeaderData += "User-Agent: Mozilla/5.0 (X11; U; Linux i686; fr; rv:1.9.0.1) Gecko/2008072820 Firefox/3.0.1\r\n";
-	HeaderData += "Referer: http://vaas3.acapela-group.com/Demo_Web/synthese_movieclip_small.swf\r\n";
-	HeaderData += "Content-type: application/x-www-form-urlencoded\r\n";
-	HeaderData += "Content-length: " + QString::number(ContentData.length()) + "\r\n";
+	QHttpRequestHeader Header;
+	Header.addValue("Host", "vaas3.acapela-group.com");
+	Header.addValue("User-Agent", "Mozilla/5.0 (X11; U; Linux i686; fr; rv:1.9.0.1) Gecko/2008072820 Firefox/3.0.1");
+	Header.addValue("Referer", "http://vaas3.acapela-group.com/Demo_Web/synthese_movieclip_small.swf");
+	Header.addValue("Content-type", "application/x-www-form-urlencoded");
+	Header.addValue("Content-length", QString::number(ContentData.length()));
+	Header.setRequest("POST", "/Demo_Web/write.php", 1, 1);
 
-	QHttp* http = new QHttp("vaas3.acapela-group.com");
-	connect(http, SIGNAL(done(bool)), this, SLOT(downloadDone()));
-
-	http->request(HeaderData, ContentData);
+	http.request(Header, ContentData);
 	loop.exec();
-	QByteArray reponse = http->readAll();
+	QByteArray reponse = http.readAll();
 	if(reponse.startsWith("&retour_php"))
 	{
 		QString acapelaFile = reponse.mid(12);
-		http->get("/Demo_Web/Sorties/" + acapelaFile + ".mp3");
+		http.get("/Demo_Web/Sorties/" + acapelaFile + ".mp3");
 		loop.exec();
 		QFile file(GlobalSettings::GetString("OpenJabNabServers/HttpPath") + fileName);
 		if (!file.open(QIODevice::WriteOnly))
@@ -44,8 +45,8 @@ bool SoundManager::createNewSound(QString text, QString voice, QString fileName)
 			Log::Error("Cannot open config file for writing");
 			return false;
 		}
-		QDataStream out(&file);
-		out << http->readAll();
+		file.write(http.readAll());
+		file.close();
 		return true;
 	}
 	Log::Error("Acapela demo did not return a sound file");
