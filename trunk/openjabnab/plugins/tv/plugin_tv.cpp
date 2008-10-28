4854cb8097799bbfdb6d496097189235d129a841
Alkorin <alkorin@openjabnab.org>
2008-10-28 12:38:42 +0000

    Added a worker thread to avoid server freeze during processing Changed the way data are stored in the plugin
    git-svn-id: https://openjabnab.svn.sourceforge.net/svnroot/openjabnab@135 1b6914c7-4944-0410-9b9d-ee7decc8b50e

--- trunk/openjabnab/plugins/tv/plugin_tv.cpp
+++ trunk/openjabnab/plugins/tv/plugin_tv.cpp
@@ -28,6 +28,11 @@ PluginTV::PluginTV():PluginInterface("tv", "Programme TV")
 	}
 }
 
+PluginTV::~PluginTV()
+{
+	Cron::UnregisterAll(this);
+}
+
 void PluginTV::OnCron(QVariant v)
 {
 	Bunny * b = QVariantHelper::ToBunnyPtr(v);
@@ -55,17 +60,118 @@ void PluginTV::getTVPage(Bunny * b)
 
 void PluginTV::analyseXml()
 {
-	QHttp * http = qobject_cast<QHttp *>(sender());
-	Bunny * b = BunnyManager::GetBunny(this, http->property("BunnyID").toByteArray());
+	std::auto_ptr<QHttp> http(qobject_cast<QHttp *>(sender()));
+	Bunny * bunny = BunnyManager::GetBunny(this, http->property("BunnyID").toByteArray());
+	if(!bunny)
+		return;
+
+	PluginTV_Worker * p = new PluginTV_Worker(this, bunny, http->readAll());
+	connect(p, SIGNAL("done(bool,Bunny*,QByteArray)"), this, SLOT("analyseDone(bool,Bunny*,QByteArray)"));
+	p->start();
+}
+
+void PluginTV::analyseDone(bool ret, Bunny * b, QByteArray message)
+{
+	if(ret && b->IsConnected())
+		 b->SendPacket(MessagePacket(message));
+
+	delete sender();
+}
+
+void PluginTV::OnBunnyConnect(Bunny * b)
+{
+	if(!webcastList.contains(b))
+	{
+		QStringList webcasts = b->GetPluginSetting(GetName(), "Webcast/List", QStringList()).toStringList();
+		foreach(QString webcast, webcasts)
+		{
+			QStringList time = webcast.split(":");
+			int id = Cron::Register(this, 60*24, time[0].toInt(), time[1].toInt(), QVariant::fromValue( b ));
+			webcastList.insert(b , qMakePair(id, webcast));
+		}
+	}
+}
+
+void PluginTV::OnBunnyDisconnect(Bunny * b)
+{
+	typedef QPair<int, QString> listElement;
+	QList<listElement> listOfWebcasts = webcastList.values(b);
+	foreach(listElement l, listOfWebcasts)
+	{
+		Cron::Unregister(this, l.first);
+	}
+	webcastList.remove(b);
+}
+
+ApiManager::ApiAnswer * PluginTV::ProcessBunnyApiCall(Bunny * b, Account const&, QString const& funcName, HTTPRequest const& r)
+{
+	if(funcName.toLower() == "addwebcast")
+	{
+		if(!r.HasArg("time"))
+			return new ApiManager::ApiError(QString("Missing argument 'time' for plugin TV"));
+	
+		if(!b->IsConnected())
+			return new ApiManager::ApiError(QString("Bunny '%1' is not connected").arg(r.GetArg("to")));
+
+		if(!b->GetPluginSetting(GetName(), "Webcast/List", QStringList()).toStringList().contains(r.GetArg("time")))
+		{
+			QStringList time = r.GetArg("time").split(":");
+			int id = Cron::Register(this, 60*24, time[0].toInt(), time[1].toInt(), QVariant::fromValue( b ));
+			webcastList.insert(b , qMakePair(id, r.GetArg("time")));
+			QStringList bunnyWebcastList = b->GetPluginSetting(GetName(), "Webcast/List", QStringList()).toStringList() << r.GetArg("time");
+			bunnyWebcastList.sort();
+			b->SetPluginSetting(GetName(), "Webcast/List", bunnyWebcastList);
+		}
+		return new ApiManager::ApiString(QString("Add webcast at '%1' to bunny '%2'").arg(r.GetArg("time"), r.GetArg("to")));
+	}
+	else if(funcName.toLower() == "removewebcast")
+	{
+		if(!r.HasArg("time"))
+			return new ApiManager::ApiError(QString("Missing argument 'time' for plugin TV"));
+	
+		if(!b->IsConnected())
+			return new ApiManager::ApiError(QString("Bunny '%1' is not connected").arg(r.GetArg("to")));
+		
+		int remove = 0;
+		QMultiMap<Bunny*, QPair<int, QString> >::iterator i = webcastList.find(b);
+		while (i != webcastList.end() && i.key() == b)
+		{
+			if(i.value().second == r.GetArg("time"))
+			{
+				Cron::Unregister(this, i->first);
+				i = webcastList.erase(i);
+				remove++;
+			}
+			else
+				++i;
+		}
+
+		QStringList bunnyWebcastList = b->GetPluginSetting(GetName(), "Webcast/List", QStringList()).toStringList();
+		bunnyWebcastList.removeAll(r.GetArg("time"));
+		bunnyWebcastList.sort();
+		b->SetPluginSetting(GetName(), "Webcast/List", bunnyWebcastList);
+		if(remove > 0)
+			return new ApiManager::ApiString(QString("Remove webcast at '%1' for bunny '%2'").arg(r.GetArg("time"), r.GetArg("to")));
+		return new ApiManager::ApiError(QString("No webcast at '%1' for bunny '%2'").arg(r.GetArg("time"), r.GetArg("to")));
+	}
+	else
+		return new ApiManager::ApiError(QString("Bad function name for plugin TV"));
+}
+
+/* WORKER THREAD */
+PluginTV_Worker::PluginTV_Worker(PluginTV * p, Bunny * bu, QByteArray b):plugin(p),bunny(bu),buffer(b.replace("&amp;", "and"))
+{
+}
+
+void PluginTV_Worker::run()
+{
 	QXmlStreamReader xml;
 	xml.clear();
-	QString source = http->readAll();
-	xml.addData(source.replace("&amp;", "and"));
-	delete http;
+	xml.addData(buffer);
 
 	QString currentTag;
 	QString chaine;
-	QByteArray message = "MU "+GetBroadcastHTTPPath("cesoir.mp3")+"\nPL 3\nMW\n";
+	QByteArray message = "MU "+plugin->GetBroadcastHTTPPath("cesoir.mp3")+"\nPL 3\nMW\n";
 	while (!xml.atEnd())
 	{
 		xml.readNext();
@@ -86,11 +192,11 @@ void PluginTV::analyseXml()
 						chaine = rx.cap(4);
 						QString chaineFile = chaine;
 						chaineFile = chaineFile.replace(" ", "").trimmed().append(".mp3").toLower();
-						Log::Debug(rx.cap(4) +" : "+rx.cap(3));
+						// Log::Debug(rx.cap(4) +" : "+rx.cap(3));
 						QByteArray fileName = QCryptographicHash::hash(rx.cap(3).trimmed().toAscii(), QCryptographicHash::Md5).toHex().append(".mp3");
-						TTSManager::CreateNewSound(rx.cap(4).trimmed(), "claire", tvFolder.absoluteFilePath(chaineFile));
-						TTSManager::CreateNewSound(rx.cap(3).trimmed(), "julie", tvFolder.absoluteFilePath(fileName));
-						message += "MU "+GetBroadcastHTTPPath(chaineFile)+"\nPL 3\nMW\nMU "+GetBroadcastHTTPPath(fileName)+"\nMW\n";
+						TTSManager::CreateNewSound(rx.cap(4).trimmed(), "claire", plugin->tvFolder.absoluteFilePath(chaineFile));
+						TTSManager::CreateNewSound(rx.cap(3).trimmed(), "julie", plugin->tvFolder.absoluteFilePath(fileName));
+						message += "MU "+plugin->GetBroadcastHTTPPath(chaineFile)+"\nPL 3\nMW\nMU "+plugin->GetBroadcastHTTPPath(fileName)+"\nMW\n";
 					}
 				}
 			}
@@ -98,89 +204,7 @@ void PluginTV::analyseXml()
 	}
 
 	if (xml.error() && xml.error() != QXmlStreamReader::PrematureEndOfDocumentError)
-	{
-		Log::Error(QString("Plugin %1 - XML Error at line %2 : %3").arg(GetVisualName()).arg(xml.lineNumber()).arg(xml.errorString()));
-	}
+		emit done(false, bunny, QByteArray());
 	else
-	{
-//		Log::Debug(message);
-		b->SendPacket(MessagePacket(message));
-	}
-}
-
-PluginTV::~PluginTV()
-{
-}
-
-void PluginTV::OnBunnyConnect(Bunny * b)
-{
-	QStringList webcasts = b->GetPluginSetting(GetName(), "Webcast/List", QStringList()).toStringList();
-	foreach(QString webcast, webcasts)
-	{
-		QStringList time = webcast.split(":");
-		int id = Cron::Register(this, 60*24, time[0].toInt(), time[1].toInt(), QVariant::fromValue( b ));
-		webcastList.insert(id, QStringList() << QString(b->GetID()) << webcast);
-	}
-}
-
-void PluginTV::OnBunnyDisconnect(Bunny * b)
-{
-	QMapIterator<int, QStringList> i(webcastList);
-	while (i.hasNext()) {
-		i.next();
-		if(i.value()[0] == QString(b->GetID()))
-			Cron::Unregister(this, i.key());
-	}
-}
-
-ApiManager::ApiAnswer * PluginTV::ProcessBunnyApiCall(Bunny * b, Account const&, QString const& funcName, HTTPRequest const& r)
-{
-	if(funcName.toLower() == "addwebcast")
-	{
-		if(!r.HasArg("time"))
-			return new ApiManager::ApiError(QString("Missing argument 'time' for plugin TV"));
-	
-		if(!b->IsConnected())
-			return new ApiManager::ApiError(QString("Bunny '%1' is not connected").arg(r.GetArg("to")));
-
-		if(!b->GetPluginSetting(GetName(), "Webcast/List", QStringList()).toStringList().contains(r.GetArg("time")))
-		{
-			QStringList time = r.GetArg("time").split(":");
-			int id = Cron::Register(this, 60*24, time[0].toInt(), time[1].toInt(), QVariant::fromValue( b ));
-			webcastList.insert(id, QStringList() << r.GetArg("to") << r.GetArg("time"));
-			QStringList bunnyWebcastList = b->GetPluginSetting(GetName(), "Webcast/List", QStringList()).toStringList() << r.GetArg("time");
-			bunnyWebcastList.sort();
-			b->SetPluginSetting(GetName(), "Webcast/List", bunnyWebcastList);
-		}
-		return new ApiManager::ApiString(QString("Add webcast at '%1' to bunny '%2'").arg(r.GetArg("time"), r.GetArg("to")));
-	}
-	else if(funcName.toLower() == "removewebcast")
-	{
-		if(!r.HasArg("time"))
-			return new ApiManager::ApiError(QString("Missing argument 'time' for plugin TV"));
-	
-		if(!b->IsConnected())
-			return new ApiManager::ApiError(QString("Bunny '%1' is not connected").arg(r.GetArg("to")));
-		
-		QMapIterator<int, QStringList> i(webcastList);
-		int remove = 0;
-		while (i.hasNext()) {
-			i.next();
-			if(i.value()[0] == r.GetArg("to") && i.value()[1] == r.GetArg("time"))
-			{
-				Cron::Unregister(this, i.key());
-				webcastList.remove(i.key());
-				remove++;
-			}
-		}
-		QStringList bunnyWebcastList = b->GetPluginSetting(GetName(), "Webcast/List", QStringList()).toStringList();
-		bunnyWebcastList.removeAll(r.GetArg("time"));
-		bunnyWebcastList.sort();
-		b->SetPluginSetting(GetName(), "Webcast/List", bunnyWebcastList);
-		if(remove > 0)
-			return new ApiManager::ApiString(QString("Remove webcast at '%1' for bunny '%2'").arg(r.GetArg("time"), r.GetArg("to")));
-		return new ApiManager::ApiError(QString("No webcast at '%1' for bunny '%2'").arg(r.GetArg("time"), r.GetArg("to")));
-	}
-	else
-		return new ApiManager::ApiError(QString("Bad function name for plugin TV"));
+		emit done(true, bunny, message);
 }
