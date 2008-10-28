70995003caa8c56378d9425b39857f856fa1d5b8
Alkorin <alkorin@openjabnab.org>
2008-10-28 12:38:42 +0000

    Added a worker thread to avoid server freeze during processing Changed the way data are stored in the plugin
    git-svn-id: https://openjabnab.svn.sourceforge.net/svnroot/openjabnab@135 1b6914c7-4944-0410-9b9d-ee7decc8b50e

--- trunk/openjabnab/plugins/tv/plugin_tv.h
+++ trunk/openjabnab/plugins/tv/plugin_tv.h
@@ -2,16 +2,20 @@
 #define _PLUGINTV_H_
 
 #include <QHttp>
+#include <QMultiMap>
 #include <QTextStream>
+#include <QThread>
 #include "plugininterface.h"
 	
 class PluginTV : public PluginInterface
 {
+	friend class PluginTV_Worker;
 	Q_OBJECT
 	Q_INTERFACES(PluginInterface)
 	
 private slots:
 	void analyseXml();
+	void analyseDone(bool, Bunny*, QByteArray);
 
 public:
 	PluginTV();
@@ -25,8 +29,27 @@ public:
 
 private:
 	void getTVPage(Bunny *);
-	QMap <int, QStringList> webcastList;
+	QMultiMap<Bunny*, QPair<int, QString> > webcastList;
 	QDir tvFolder;
+
+};
+
+class PluginTV_Worker : public QThread
+{
+	Q_OBJECT
+
+signals:
+	void done(bool, Bunny*, QByteArray);
+
+public:
+	PluginTV_Worker(PluginTV * , Bunny *, QByteArray);
+	virtual ~PluginTV_Worker() {}
+	void run();
+
+private:
+	PluginTV * plugin;
+	Bunny * bunny;
+	QByteArray buffer;
 
 };
 
