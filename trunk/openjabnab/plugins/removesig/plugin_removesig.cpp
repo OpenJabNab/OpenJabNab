02ae8345050999e1d62bb5b43d676e8973d82604
Pixel166 <alexis.mellone@gadz.org>
2008-11-05 13:56:22 +0000

    Added some settings (choose what to remove and what to keep) Added removing of 'Respiration'
    git-svn-id: https://openjabnab.svn.sourceforge.net/svnroot/openjabnab@153 1b6914c7-4944-0410-9b9d-ee7decc8b50e

--- trunk/openjabnab/plugins/removesig/plugin_removesig.cpp
+++ trunk/openjabnab/plugins/removesig/plugin_removesig.cpp
@@ -14,12 +14,23 @@ PluginRemoveSig::PluginRemoveSig():PluginInterface("removesig", "Removes signatu
 
 bool PluginRemoveSig::XmppVioletPacketMessage(Bunny * b, Packet const& p)
 {
-	QRegExp rx("MC broadcast/[a-z_/]*signature.mp3\nCL \\d+\nCH broadcast/[a-z_/]*\\.chor\nMW\n");
-	if(QString(p.GetPrintableData()).indexOf(rx) != -1)
+	QRegExp rx1("MC broadcast/[a-z_/]*signature[a-z_/]*.mp3\nCL \\d+\nCH broadcast/[a-z_/]*\\.chor\nMW\n");
+	bool signature = b->GetPluginSetting(GetName(), "Remove/Signature", true).toBool();
+
+	QRegExp rx2("MC broadcast/[a-z_/]*/respiration/\\d+Respiration\\d+\\.mp3\nMW\n");
+	bool respiration = b->GetPluginSetting(GetName(), "Remove/Respiration", true).toBool();
+
+	if(
+		(QString(p.GetPrintableData()).indexOf(rx1) != -1 && signature) ||
+		(QString(p.GetPrintableData()).indexOf(rx2) != -1 && respiration)
+		)
 	{
 		QString packet = p.GetPrintableData();
 		Log::Debug(QString("Before removing signature :\n%1").arg(packet));
-		packet = packet.remove(rx);
+		if(signature)
+			packet = packet.remove(rx1);
+		if(respiration)
+			packet = packet.remove(rx2);
 		Log::Debug(QString("After removing signature :\n%1").arg(packet));
 		b->SendPacket(MessagePacket(packet.toAscii()));
 		return true;
@@ -27,6 +38,24 @@ bool PluginRemoveSig::XmppVioletPacketMessage(Bunny * b, Packet const& p)
 	return false;
 }
 
-PluginRemoveSig::~PluginRemoveSig()
+PluginRemoveSig::~PluginRemoveSig() {}
+
+ApiManager::ApiAnswer * PluginRemoveSig::ProcessBunnyApiCall(Bunny * b, Account const&, QString const& funcName, HTTPRequest const& r)
 {
+	if(funcName.toLower() == "signature")
+	{
+		if(!r.HasArg("remove"))
+			return new ApiManager::ApiError(QString("Missing argument 'remove' for plugin Remove signature"));
+		b->SetPluginSetting(GetName(), "Remove/Signature", QVariant(r.GetArg("remove")));
+		return new ApiManager::ApiOk(QString("Plugin configuration updated."));
+	}
+	else if(funcName.toLower() == "respiration")
+	{
+		if(!r.HasArg("remove"))
+			return new ApiManager::ApiError(QString("Missing argument 'remove' for plugin Remove signature"));
+		b->SetPluginSetting(GetName(), "Remove/Respiration", QVariant(r.GetArg("remove")));
+		return new ApiManager::ApiOk(QString("Plugin configuration updated."));
+	}
+	else
+		return new ApiManager::ApiError(QString("Bad function name for plugin Remove signature"));
 }
