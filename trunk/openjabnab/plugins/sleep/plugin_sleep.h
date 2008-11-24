b9a22de79b2d8ea36afa46b9fa973af168573668
Pixel166 <pixel@openjabnab.org>
2008-11-24 12:24:38 +0000

    Non working version of the plugin
    git-svn-id: https://openjabnab.svn.sourceforge.net/svnroot/openjabnab@167 1b6914c7-4944-0410-9b9d-ee7decc8b50e

--- trunk/openjabnab/plugins/sleep/plugin_sleep.h
+++ trunk/openjabnab/plugins/sleep/plugin_sleep.h
@@ -1,9 +1,19 @@
 #ifndef _PLUGINSLEEP_H_
 #define _PLUGINSLEEP_H_
 
+#include <QList>
+#include <QMap>
+#include <QPair>
+#include <QStringList>
+#include <QTime>
 #include "plugininterface.h"
 #include "httprequest.h"
 	
+typedef struct {
+	QTime listOfTimes[7];
+	int listOfCrons[7];
+} Week;
+
 class PluginSleep : public PluginInterface
 {
 	Q_OBJECT
@@ -12,9 +22,19 @@ class PluginSleep : public PluginInterface
 public:
 	PluginSleep();
 	virtual ~PluginSleep();
+	void OnCronSleep();
+	void OnCronWakeUp();
+	void OnBunnyConnect(Bunny *);
+	void OnBunnyDisconnect(Bunny *);
 
 	ApiManager::ApiAnswer * ProcessBunnyApiCall(Bunny *, Account const&, QString const&, HTTPRequest const&);
-	
+
+private:
+
+	void RegisterCrons(Bunny *);
+	QList< int > listOfCrons;
+	QMap<Bunny *, Week *> listOfSleep;
+	QMap<Bunny *, Week *> listOfWakeUp;
 };
 
 #endif
