0e6d7fd3caf1fcfe9e90a68095754d110cc882fe
Pixel166 <pixel@openjabnab.org>
2009-08-27 17:53:35 +0000

    Change TTSManager to use new version of Acapela demo
    git-svn-id: https://openjabnab.svn.sourceforge.net/svnroot/openjabnab@170 1b6914c7-4944-0410-9b9d-ee7decc8b50e

--- trunk/openjabnab/lib/ttsmanager.cpp
+++ trunk/openjabnab/lib/ttsmanager.cpp
@@ -37,12 +37,12 @@ bool TTSManager::CreateNewSound(QString text, QString voice, QString fileName, b
 	QObject::connect(&http, SIGNAL(done(bool)), &loop, SLOT(quit()));
 
 	QByteArray ContentData;
-	ContentData += "client%5Ftext=" + QUrl::toPercentEncoding(text) + "&client%5Fvoice=" + voice + "22k&client%5Frequest%5Ftype=CREATE%5FREQUEST&client%5Fpassword=reuno&client%5Flogin=asTTS&client%5Fversion=1%2D00&onLoad=%5Btype%20Function%5D";
+	ContentData += "client%5Ftext=" + QUrl::toPercentEncoding(text) + "&client%5Fvoice=" + voice + "22k&client%5Frequest%5Ftype=CREATE%5FREQUEST&client%5Fpassword=demo_web&client%5Flogin=asTTS&client%5Fversion=1%2D00&actionscript%5Fversion=3";
 
 	QHttpRequestHeader Header;
 	Header.addValue("Host", "vaas3.acapela-group.com");
 	Header.addValue("User-Agent", "Mozilla/5.0 (X11; U; Linux i686; fr; rv:1.9.0.1) Gecko/2008072820 Firefox/3.0.1");
-	Header.addValue("Referer", "http://vaas3.acapela-group.com/Demo_Web/synthese_movieclip_small.swf");
+	Header.addValue("Referer", "http://www.acapela-group.com/Flash/Demo_Web_AS3/demo_web.swf?path=http://vaas3.acapela-group.com/connector/1-20/&lang=FR");
 	Header.addValue("Content-type", "application/x-www-form-urlencoded");
 	Header.addValue("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8");
 	Header.addValue("Accept-Language", "fr,fr-fr;q=0.8,en-us;q=0.5,en;q=0.3");
@@ -52,18 +52,19 @@ bool TTSManager::CreateNewSound(QString text, QString voice, QString fileName, b
 	Header.addValue("Connection", "keep-alive");
 
 	Header.setContentLength(ContentData.length());
-	Header.setRequest("POST", "/asTTS/v1-00/textToMP3.php", 1, 1);
+	Header.setRequest("POST", "/connector/1-20/textToMP3.php", 1, 1);
 
 	http.request(Header, ContentData);
 	loop.exec();
 	QByteArray reponse = http.readAll();
+	Log::Info(reponse);
 	QUrl url("?"+reponse);
 	if(url.hasQueryItem("retour_php"))
 	{
 		Log::Debug(QString("Acapela answer : %1").arg(QString(reponse)));
 		QString acapelaFile = url.queryItemValue("retour_php");
 		Log::Info(QString("Downloading MP3 file : %1").arg(QString(acapelaFile)));
-		http.get(QString("/asTTS/v1-00/%1").arg(acapelaFile));
+		http.get(acapelaFile);
 		loop.exec();
 		QFile file(fileName);
 		if (!file.open(QIODevice::WriteOnly))
