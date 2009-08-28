bb4575c7f355caa951e09f1bf52581c45812ebcb
Pixel166 <pixel@openjabnab.org>
2009-08-28 12:56:58 +0000

    Add Standalone mode
    git-svn-id: https://openjabnab.svn.sourceforge.net/svnroot/openjabnab@174 1b6914c7-4944-0410-9b9d-ee7decc8b50e

--- trunk/openjabnab/plugins/system/locate/plugin_locate.cpp
+++ trunk/openjabnab/plugins/system/locate/plugin_locate.cpp
@@ -22,24 +22,26 @@ bool PluginLocate::HttpRequestHandle(HTTPRequest & request)
 		
 		Log::Info(QString("Requesting LOCATE for tag %1").arg(serialnumber));
 		
-		// Forward request to Violet
-		QByteArray violetAnswer = request.ForwardTo(GlobalSettings::GetString("DefaultVioletServers/BootServer"));
-
-		// Analyse the answer to see if servers has changed
-		QList<QByteArray> lines = violetAnswer.split('\n');
-		foreach(QByteArray line, lines)
+		if(GlobalSettings::Get("Config/StandAlone", true) == false)
 		{
-			if (line.startsWith("ping")) 
-			{}
-			else if (line.startsWith("broad"))
-			{}
-			else if (line.startsWith("xmpp_domain"))
-			{}
-			else
-				if (line.size() != 0)
-					Log::Error(QString("Unknown locate server : ") + line);
-		}
+			// Forward request to Violet
+			QByteArray violetAnswer = request.ForwardTo(GlobalSettings::GetString("DefaultVioletServers/BootServer"));
 
+			// Analyse the answer to see if servers has changed
+			QList<QByteArray> lines = violetAnswer.split('\n');
+			foreach(QByteArray line, lines)
+			{
+				if (line.startsWith("ping")) 
+				{}
+				else if (line.startsWith("broad"))
+				{}
+				else if (line.startsWith("xmpp_domain"))
+				{}
+				else
+					if (line.size() != 0)
+						Log::Error(QString("Unknown locate server : ") + line);
+			}
+		}
 		QString locateString;
 		locateString += QString("ping %1\n").arg(GlobalSettings::GetString("OpenJabNabServers/PingServer"));
 		locateString += QString("broad %1\n").arg(GlobalSettings::GetString("OpenJabNabServers/BroadServer"));
