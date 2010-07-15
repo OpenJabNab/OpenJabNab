57c9270e88e945ed2d104989370b3577c5e58bec
Alkorin <alkorin@openjabnab.org>
2010-07-15 21:05:07 +0000

    Reverted changes in autoprepend.php ...
    git-svn-id: https://openjabnab.svn.sourceforge.net/svnroot/openjabnab@217 1b6914c7-4944-0410-9b9d-ee7decc8b50e

--- trunk/http-wrapper/ojn_admin/include/autoprepend.php
+++ trunk/http-wrapper/ojn_admin/include/autoprepend.php
@@ -1,7 +1,7 @@
 <?
 define('ROOT_SITE', realpath(dirname(__FILE__)."/../")."/");
-define('ROOT_WWW_ADMIN', 'http://ojn.soete.org/ojn_admin/');
-define('ROOT_WWW_API', 'http://ojn.soete.org/ojn_api/');
+define('ROOT_WWW_ADMIN', 'http://<HOSTNAME>/ojn_admin/');
+define('ROOT_WWW_API', 'http://<HOSTNAME>/ojn_api/');
 
 session_start('openJabNab');
 require_once(ROOT_SITE.'class/api.class.php');
