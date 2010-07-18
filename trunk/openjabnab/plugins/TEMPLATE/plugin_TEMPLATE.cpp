af03f4069b00c91ed6df7183de8d242e7cd531da
Alkorin <alkorin@openjabnab.org>
2010-07-18 17:33:45 +0000

    TEMPLATE Plugin
    git-svn-id: https://openjabnab.svn.sourceforge.net/svnroot/openjabnab@228 1b6914c7-4944-0410-9b9d-ee7decc8b50e

---
+++ trunk/openjabnab/plugins/TEMPLATE/plugin_TEMPLATE.cpp
@@ -0,0 +1,21 @@
+#include "plugin_TEMPLATELOWER.h"
+
+Q_EXPORT_PLUGIN2(plugin_TEMPLATELOWER, TEMPLATECLASS)
+
+TEMPLATECLASS::TEMPLATECLASS():PluginInterface("template", "template plugin")
+{
+}
+
+TEMPLATECLASS::~TEMPLATECLASS() {}
+
+void TEMPLATECLASS::OnBunnyConnect(Bunny *)
+{
+}
+
+void TEMPLATECLASS::OnBunnyDisconnect(Bunny *)
+{
+}
+
+void TEMPLATECLASS::OnInitPacket(const Bunny *, AmbientPacket &, SleepPacket &)
+{
+}
