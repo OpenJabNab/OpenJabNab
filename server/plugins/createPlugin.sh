fc6c12e33a76b892fcfe93f9a6e374b60cceb012
Alkorin <alkorin@openjabnab.org>
2010-09-06 16:55:17 +0200

    Fixed bug in createPlugin.sh

--- server/plugins/createPlugin.sh
+++ server/plugins/createPlugin.sh
@@ -7,6 +7,7 @@ fi
 
 pluginLower=`echo $1 | tr [:upper:] [:lower:]`
 pluginClassName="Plugin"`echo ${1:0:1} | tr [:lower:] [:upper:]`${1:1:${#1}};
+pluginClassNameUpper=`echo $pluginClassName | tr [:lower:] [:upper:]`;
 
 echo "Creating '$pluginClassName' from template in '$pluginLower' folder ..."
 
@@ -21,5 +22,6 @@ cp TEMPLATE/plugin_TEMPLATE.cpp $pluginLower/plugin_$pluginLower.cpp
 cp TEMPLATE/plugin_TEMPLATE.h $pluginLower/plugin_$pluginLower.h
 cp TEMPLATE/TEMPLATE.pro $pluginLower/$pluginLower.pro
 
+sed -i -s "s/TEMPLATECLASSUPPER/$pluginClassNameUpper/g" $pluginLower/*
 sed -i -s "s/TEMPLATECLASS/$pluginClassName/g" $pluginLower/*
 sed -i -s "s/TEMPLATELOWER/$pluginLower/g" $pluginLower/*
