37849f0697a75f6960b1469d864f77df2e31eda9
Pixel166 <pixel@openjabnab.org>
2011-04-17 23:35:47 +0200

    New plugin to change breathing color (blinking led under the bunny)

---
+++ server/plugins/colorbreathing/plugin_colorbreathing.h
@@ -0,0 +1,29 @@
+#ifndef _PLUGINCOLORBREATHING_H_
+#define _PLUGINCOLORBREATHING_H_
+
+#include "plugininterface.h"
+#include "httprequest.h"
+	
+class PluginColorbreathing : public PluginInterface
+{
+	Q_OBJECT
+	Q_INTERFACES(PluginInterface)
+
+public:
+	PluginColorbreathing();
+	virtual ~PluginColorbreathing() {};
+	void patchBootcode(HTTPRequest &, long, int, QByteArray, QByteArray);
+	void patchBootcode(HTTPRequest &, long, char, char);
+	void HttpRequestAfter(HTTPRequest &);
+        void InitApiCalls();
+
+protected:
+        PLUGIN_BUNNY_API_CALL(Api_GetColorList);
+        PLUGIN_BUNNY_API_CALL(Api_SetColor);
+        PLUGIN_BUNNY_API_CALL(Api_GetColor);
+
+        QStringList availableColors;
+
+};
+
+#endif
