50ef77e1ea3d6d0f629c94cfe0f35e012308b436
RedoXPS <dev@redox.ws>
2011-04-28 19:33:38 +0200

    Fix Packet plugin, Add Mandatory reload parameter to some API calls

--- http-wrapper/ojn_admin/class/template.class.php
+++ http-wrapper/ojn_admin/class/template.class.php
@@ -7,15 +7,14 @@ class ojnTemplate {
 	
 	public function ojnTemplate($api) {
 		$this->Api=$api;
-		//var_dump($_SERVER);
 	}
 
 	public function display($buffer) {
 		$template = file_get_contents(ROOT_SITE.'class/template.tpl.php');
-		$ListOfConnectedBunnies = $this->Api->getListOfConnectedBunnies();
-		$ListOfBunnies = $this->Api->getListOfBunnies();
-		$ListOfPlugins = $this->Api->getListOfPlugins();
-		$ListOfActivePlugins = $this->Api->getListOfActivePlugins();
+		$ListOfConnectedBunnies = $this->Api->getListOfConnectedBunnies(false);
+		$ListOfBunnies = $this->Api->getListOfBunnies(false);
+		$ListOfPlugins = $this->Api->getListOfPlugins(false);
+		$ListOfActivePlugins = $this->Api->getListOfActivePlugins(false);
 		
 		if(empty($ListOfPlugins) && isset($_SESSION['connected']) 
 		   && !isset($_POST['login']) && !strpos($_SERVER['REQUEST_URI'],"logout"))
@@ -59,8 +58,8 @@ class ojnTemplate {
 
 	function makeBunnyMenu()	{
 		$bunny = "";
-		$online = $this->Api->getListOfConnectedBunnies();
-		$bunnies = $this->Api->getListOfBunnies();
+		$online = $this->Api->getListOfConnectedBunnies(false);
+		$bunnies = $this->Api->getListOfBunnies(false);
 		if(!empty($bunnies))
 			foreach($bunnies as $mac => $bunny)
 				$menu .= '<li'.(isset($online[$mac]) ? ' class="online"' : '').'><a href="bunny.php?b='.$mac.'" alt="'.$mac.'" title="'.$mac.'">'.($bunny != "Bunny" ? $bunny : $mac).'</a></li>';
