73b4c3a8d91281c290dd904c834a58f3e07e05ac
Pixel166 <pixel@openjabnab.org>
2011-05-03 12:26:04 +0200

    Added a logo in admin

--- http-wrapper/ojn_admin/class/template.tpl.php
+++ http-wrapper/ojn_admin/class/template.tpl.php
@@ -22,7 +22,7 @@
       </div>
 
       <div class="midHeader">
-        <h1 class="headerTitle" alt="<!!ALTTITLE!!>" title="<!!ALTTITLE!!>"><!!TITLE!!></h1>
+        <h1 class="headerTitle" alt="<!!ALTTITLE!!>" title="<!!ALTTITLE!!>"><img src="/ojn_admin/images/logo.png"></h1>
         <div class="headerSubTitle"><!!SUBTITLE!!></div>
 
         <div class="headerLinks">
