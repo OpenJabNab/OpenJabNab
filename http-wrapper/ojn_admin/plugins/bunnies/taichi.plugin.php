291cd95c004394576f0f1ec2b420e398836b3328
stabilo <leolancia@gmail.com>
2011-07-20 07:51:11 -0700

    Added a selector for the tai chi frequency

--- http-wrapper/ojn_admin/plugins/bunnies/taichi.plugin.php
+++ http-wrapper/ojn_admin/plugins/bunnies/taichi.plugin.php
@@ -7,10 +7,11 @@ if(!empty($_POST['frequency'])) {
 $frequency = $ojnAPI->getApiString("bunny/".$_SESSION['bunny']."/taichi/getFrequency?".$ojnAPI->getToken());
 $frequency = isset($frequency['value']) ? $frequency['value'] : '';
 ?>
+
 <form method="post">
 <fieldset>
-<legend>Frequence du Taichi</legend>
-<input type="text" value="<?php echo $frequency ?>" name="frequency">
+<legend>Votre lapin aime le Tai-Chi ? </legend>
+<select name="frequency"> <option value="50" <?php if ($frequency==50) echo 'selected'; ?> >Un peu...</option><option value="125" <?php if ($frequency==125) echo 'selected'; ?>>Beaucoup...</option><option value="250" <?php if ($frequency==250) echo 'selected'; ?>>A la folie...</option><option value="0" <?php if ($frequency==0) echo 'selected'; ?>>Pas du tout!</option></select>
 <input type="submit" value="Enregistrer">
 </fieldset>
 </form>
