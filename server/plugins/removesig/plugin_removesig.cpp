74199f8ef2785ac51ad587ce5b04c657c8fe6057
Pixel166 <alexis.mellone@gadz.org>
2011-07-25 17:00:22 +0200

    Remove debug (plugin is stable)

--- server/plugins/removesig/plugin_removesig.cpp
+++ server/plugins/removesig/plugin_removesig.cpp
@@ -26,12 +26,10 @@ bool PluginRemoveSig::XmppVioletPacketMessage(Bunny * b, Packet const& p)
 		)
 	{
 		QString packet = p.GetPrintableData();
-		LogDebug(QString("Before removing signature :\n%1").arg(packet));
 		if(signature)
 			packet = packet.remove(rx1);
 		if(respiration)
 			packet = packet.remove(rx2);
-		LogDebug(QString("After removing signature :\n%1").arg(packet));
 		b->SendPacket(MessagePacket(packet.toAscii()));
 		return true;
 	}
