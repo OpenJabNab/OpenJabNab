93b163ba7bb4110fc8962076f7864bcb964e9269
Alkorin <alkorin@openjabnab.org>
2011-08-08 14:49:14 +0200

    Remake indentation of ttsmanager.cpp. We use tabs :-)

--- server/lib/ttsmanager.cpp
+++ server/lib/ttsmanager.cpp
@@ -15,119 +15,117 @@
 
 TTSManager::TTSManager()
 {
-		// Load all tts
-		ttsDir = QCoreApplication::applicationDirPath();
-		if (!ttsDir.cd("tts"))
-		{
-			LogError("Unable to open tts directory !\n");
-			exit(-1);
-		}
+	ttsDir = QCoreApplication::applicationDirPath();
+	ttsDir.setNameFilters(QStringList("*.so"));
+
+	if (!ttsDir.cd("tts"))
+	{
+		LogError("Unable to open tts directory !\n");
+		exit(-1);
+	}
 }
 
 TTSManager & TTSManager::Instance()
 {
-  static TTSManager p;
-  return p;
+	static TTSManager p;
+	return p;
 }
 
 void TTSManager::UnloadTTSs()
 {
-        foreach(TTSInterface * p, listOfTTSs)
-			delete p;
+	foreach(TTSInterface * p, listOfTTSs)
+		delete p;
 
-        foreach(QPluginLoader * l, listOfTTSsLoader.values())
-        {
-                l->unload();
-                delete l;
-        }
+	foreach(QPluginLoader * l, listOfTTSsLoader.values())
+	{
+		l->unload();
+		delete l;
+	}
 }
 
 void TTSManager::LoadTTSs()
 {
-        LogInfo(QString("Finding tts in : %1").arg(ttsDir.path()));
-		QStringList filters;
-		filters << "*.so";
-		ttsDir.setNameFilters(filters);
-		foreach (QFileInfo file, ttsDir.entryInfoList(QDir::Files))
-			LoadTTS(file.fileName().toAscii());
+	LogInfo(QString("Finding tts in : %1").arg(ttsDir.path()));
+	foreach (QFileInfo file, ttsDir.entryInfoList(QDir::Files))
+		LoadTTS(file.fileName().toAscii());
 }
 
 bool TTSManager::LoadTTS(QString const& fileName)
 {
-        if(listOfTTSsByFileName.contains(fileName))
-        {
-                LogError(QString("TTS '%1' already loaded !").arg(fileName));
-                return false;
-        }
+	if(listOfTTSsByFileName.contains(fileName))
+	{
+		LogError(QString("TTS '%1' already loaded !").arg(fileName));
+		return false;
+	}
 
-        QString file = ttsDir.absoluteFilePath(fileName);
-        if (!QLibrary::isLibrary(file))
-                return false;
+	QString file = ttsDir.absoluteFilePath(fileName);
+	if (!QLibrary::isLibrary(file))
+		return false;
 
-        QString status = QString("Loading %1 : ").arg(fileName);
+	QString status = QString("Loading %1 : ").arg(fileName);
 
-        QPluginLoader * loader = new QPluginLoader(file);
-        QObject * p = loader->instance();
-        TTSInterface * tts = qobject_cast<TTSInterface *>(p);
-        if (tts)
-        {
-                if(tts->Init() == false)
-                {
-                        delete tts;
-                        loader->unload();
-                        delete loader;
+	QPluginLoader * loader = new QPluginLoader(file);
+	QObject * p = loader->instance();
+	TTSInterface * tts = qobject_cast<TTSInterface *>(p);
+	if (tts)
+	{
+		if(tts->Init() == false)
+		{
+			delete tts;
+			loader->unload();
+			delete loader;
 
-                        status.append(QString("%1 OK, Initialisation failed").arg(tts->GetName()));
-                        LogInfo(status);
-                        return false;
-                }
+			status.append(QString("%1 OK, Initialisation failed").arg(tts->GetName()));
+			LogInfo(status);
+			return false;
+		}
 
-                listOfTTSs.append(tts);
-                listOfTTSsFileName.insert(tts, fileName);
-                listOfTTSsLoader.insert(tts, loader);
-                listOfTTSsByName.insert(tts->GetName(), tts);
-                listOfTTSsByFileName.insert(fileName, tts);
+		listOfTTSs.append(tts);
+		listOfTTSsFileName.insert(tts, fileName);
+		listOfTTSsLoader.insert(tts, loader);
+		listOfTTSsByName.insert(tts->GetName(), tts);
+		listOfTTSsByFileName.insert(fileName, tts);
 
-                status.append(QString("%1 OK, Enable : %2").arg(tts->GetName(),tts->GetEnable() ? "Yes" : "No"));
-                LogInfo(status);
-                return true;
-        }
-        status.append("Failed, ").append(loader->errorString());
-        LogInfo(status);
-        return false;
+		status.append(QString("%1 OK, Enable : %2").arg(tts->GetName(),tts->GetEnable() ? "Yes" : "No"));
+		LogInfo(status);
+		return true;
+	}
+	status.append("Failed, ").append(loader->errorString());
+	LogInfo(status);
+	return false;
 }
 
 bool TTSManager::UnloadTTS(QString const& name)
 {
-        if(listOfTTSsByName.contains(name))
-        {
-                TTSInterface * p = listOfTTSsByName.value(name);
-                QString fileName = listOfTTSsFileName.value(p);
-                QPluginLoader * loader = listOfTTSsLoader.value(p);
-                listOfTTSsByFileName.remove(fileName);
-                listOfTTSsFileName.remove(p);
-                listOfTTSsLoader.remove(p);
-                listOfTTSsByName.remove(name);
-                listOfTTSs.removeAll(p);
-                delete p;
-                loader->unload();
-                delete loader;
-                LogInfo(QString("TTS %1 unloaded.").arg(name));
-                return true;
-        }
-        LogInfo(QString("Can't unload tts %1").arg(name));
-        return false;
+	if(listOfTTSsByName.contains(name))
+	{
+		TTSInterface * p = listOfTTSsByName.value(name);
+		QString fileName = listOfTTSsFileName.value(p);
+		QPluginLoader * loader = listOfTTSsLoader.value(p);
+		listOfTTSsByFileName.remove(fileName);
+		listOfTTSsFileName.remove(p);
+		listOfTTSsLoader.remove(p);
+		listOfTTSsByName.remove(name);
+		listOfTTSs.removeAll(p);
+		delete p;
+		loader->unload();
+		delete loader;
+		LogInfo(QString("TTS %1 unloaded.").arg(name));
+		return true;
+	}
+	LogInfo(QString("Can't unload tts %1").arg(name));
+	return false;
 }
 
 bool TTSManager::ReloadTTS(QString const& name)
 {
-        if(listOfTTSsByName.contains(name))
-        {
-                TTSInterface * p = listOfTTSsByName.value(name);
-                QString file = listOfTTSsFileName.value(p);
-                return (UnloadTTS(name) && LoadTTS(file));
-        }
-        return false;
+	if(listOfTTSsByName.contains(name))
+	{
+		TTSInterface * p = listOfTTSsByName.value(name);
+		QString file = listOfTTSsFileName.value(p);
+		return (UnloadTTS(name) && LoadTTS(file));
+	}
+	return false;
 }
 
 // Creatte TTS Song in /broadcast/tts/<name>/<voice>/[md5].mp3
