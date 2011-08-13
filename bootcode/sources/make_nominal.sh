5aa025da7ca3942fb62b4524d15249b9e2b3abbf
Alkorin <alkorin@openjabnab.org>
2011-08-13 14:47:48 +0200

    Remove old files

--- bootcode/sources/make_nominal.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-CURRENTDIR=$(dirname $0)
-cd $CURRENTDIR
-
-PREPROC="../preproc.pl"
-PREPROC2="../preproc_remove_extra_protos.pl"
-
-"$PREPROC" < main.mtl | "$PREPROC2" > "../nominal.mtl"
