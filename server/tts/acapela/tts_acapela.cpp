d1ef8e1517c77c97736fca74205def3d638a3daf
Pixel166 <alexis.mellone@gadz.org>
2011-09-05 10:51:20 +0200

    Update Acapela plugin, which stopped working a few days ago...

--- server/tts/acapela/tts_acapela.cpp
+++ server/tts/acapela/tts_acapela.cpp
@@ -38,13 +38,13 @@ QByteArray TTSAcapela::CreateNewSound(QString text, QString voice, bool forceOve
 	QDir outputFolder = ttsFolder;
 	if(!outputFolder.exists(voice))
 		outputFolder.mkdir(voice);
-	
+
 	if(!outputFolder.cd(voice))
 	{
 		LogError(QString("Cant create TTS Folder : %1").arg(ttsFolder.absoluteFilePath(voice)));
 		return QByteArray();
 	}
-	
+
 	// Compute fileName
 	QString fileName = QCryptographicHash::hash(text.toAscii(), QCryptographicHash::Md5).toHex().append(".mp3");
 	QString filePath = outputFolder.absoluteFilePath(fileName);
@@ -53,17 +53,17 @@ QByteArray TTSAcapela::CreateNewSound(QString text, QString voice, bool forceOve
 		return ttsHTTPUrl.arg(voice, fileName).toAscii();
 
 	// Fetch MP3
-	QHttp http("vaas3.acapela-group.com");
+	QHttp http("vaas.acapela-group.com");
 	QObject::connect(&http, SIGNAL(done(bool)), &loop, SLOT(quit()));
 
 	QByteArray ContentData;
-	ContentData += "client%5Ftext=" + QUrl::toPercentEncoding(text) + "&client%5Fvoice=" + voice + "22k&client%5Frequest%5Ftype=CREATE%5FREQUEST&client%5Fpassword=demo_web&client%5Flogin=asTTS&client%5Fversion=1%2D00&actionscript%5Fversion=3";
+	ContentData += "client%5Ftext=" + QUrl::toPercentEncoding(text) + "&client%5Fvoice=" + voice + "22k&client%5Frequest%5Ftype=CREATE%5FREQUEST&client%5Fpassword=demo%5Fweb&client%5Flogin=asTTS&client%5Fversion=1%2D00&actionscript%5Fversion=3";
 
 	QHttpRequestHeader Header;
-	Header.addValue("Host", "vaas3.acapela-group.com");
-	Header.addValue("User-Agent", "Mozilla/5.0 (X11; U; Linux i686; fr; rv:1.9.0.1) Gecko/2008072820 Firefox/3.0.1");
-	Header.addValue("Referer", "http://www.acapela-group.com/Flash/Demo_Web_AS3/demo_web.swf?path=http://vaas3.acapela-group.com/connector/1-20/&lang=FR");
-	Header.addValue("Content-type", "application/x-www-form-urlencoded"); 
+	Header.addValue("Host", "vaas.acapela-group.com");
+	Header.addValue("User-Agent", "Mozilla/5.0 (X11; U; Linux i686; fr; rv:1.9.0.1) Gecko/2011000000 Firefox/6.0.1");
+	Header.addValue("Referer", "http://www.acapela-group.com/Flash/Demo_Web_AS3/demo_web.swf?path=http://vaas.acapela-group.com/Services/DemoWeb/&lang=EN");
+	Header.addValue("Content-type", "application/x-www-form-urlencoded");
 	Header.addValue("Accept", "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8");
 	Header.addValue("Accept-Language", "fr,fr-fr;q=0.8,en-us;q=0.5,en;q=0.3");
 	Header.addValue("Accept-Encoding", "gzip,deflate");
@@ -72,18 +72,20 @@ QByteArray TTSAcapela::CreateNewSound(QString text, QString voice, bool forceOve
 	Header.addValue("Connection", "keep-alive");
 
 	Header.setContentLength(ContentData.length());
-	Header.setRequest("POST", "/connector/1-20/textToMP3.php", 1, 1);
-
+	Header.setRequest("POST", "/Services/DemoWeb/textToMP3.php", 1, 1);
 	http.request(Header, ContentData);
 	loop.exec();
 	QByteArray reponse = http.readAll();
 	QUrl url("?"+reponse);
-	if(url.hasQueryItem("retour_php"))
+	if(reponse.startsWith("retour_php"))
 	{
 		//LogDebug(QString("Acapela answer : %1").arg(QString(reponse)));
 		QString acapelaFile = url.queryItemValue("retour_php");
+		QUrl urlfile(acapelaFile);
+		http.setHost(urlfile.host());
+		Header.setRequest("GET",urlfile.path(),1,1);
 		//LogInfo(QString("Downloading MP3 file : %1").arg(QString(acapelaFile)));
-		http.get(acapelaFile);
+		http.request(Header,"");
 		loop.exec();
 		QFile file(filePath);
 		if (!file.open(QIODevice::WriteOnly))
