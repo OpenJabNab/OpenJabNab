bbc19cdbadda685e6fab1735515f5b1f1ff429cc
Pixel166 <alexis.mellone@gadz.org>
2011-09-23 11:03:05 +0200

    Add functions + API to search in the directory

--- server/plugins/system/annuaire/plugin_annuaire.cpp
+++ server/plugins/system/annuaire/plugin_annuaire.cpp
@@ -1,4 +1,7 @@
 #include <QHttp>
+#include <QDebug>
+#include <QUrl>
+#include <QXmlStreamReader>
 #include "bunny.h"
 #include "account.h"
 #include "plugin_annuaire.h"
@@ -20,6 +23,99 @@ void PluginAnnuaire::OnBunnyConnect(Bunny * b)
 	}
 }
 
+QList<BunnyInfos> PluginAnnuaire::SearchBunnyByName(QString name)
+{
+	QEventLoop loop;
+
+	QHttp *http = new QHttp(GetSettings("global/URL", "").toString(), 80);
+	http->get("/whois.php?n=" + QUrl::toPercentEncoding(name));
+	QObject::connect(http, SIGNAL(done(bool)), &loop, SLOT(quit()));
+	loop.exec();
+
+	QXmlStreamReader xml;
+	xml.clear();
+	xml.addData(http->readAll());
+
+	QString currentTag;
+	QList<BunnyInfos> whois = QList<BunnyInfos>();
+	BunnyInfos currentBunny;
+	while (!xml.atEnd())
+	{
+		xml.readNext();
+		if (xml.isStartElement() || xml.isEndElement())
+		{
+			currentTag = xml.name().toString();
+		}
+		else if (xml.isCharacters() && !xml.isWhitespace())
+		{
+			if(currentTag == "name")
+			{
+				currentBunny.name = xml.text().toString();
+			}
+			else if(currentTag == "macaddress")
+			{
+				currentBunny.ID = xml.text().toString().toAscii();
+			}
+			else if(currentTag == "server")
+			{
+				currentBunny.server = xml.text().toString();
+			}
+		}
+		if(xml.isEndElement() && currentTag == "bunny")
+		{
+			whois.append(currentBunny);
+		}
+	}
+
+	return whois;
+}
+
+QList<BunnyInfos> PluginAnnuaire::SearchBunnyByMac(QByteArray ID)
+{
+	QEventLoop loop;
+
+	QHttp *http = new QHttp(GetSettings("global/URL", "").toString(), 80);
+	http->get("/whois.php?nm" + QUrl::toPercentEncoding(QString(ID)));
+	QObject::connect(http, SIGNAL(done(bool)), &loop, SLOT(quit()));
+	loop.exec();
+
+	QXmlStreamReader xml;
+	xml.clear();
+	xml.addData(http->readAll());
+
+	QString currentTag;
+	QList<BunnyInfos> whois = QList<BunnyInfos>();
+	BunnyInfos currentBunny;
+	while (!xml.atEnd())
+	{
+		xml.readNext();
+		if (xml.isStartElement() || xml.isEndElement())
+		{
+			currentTag = xml.name().toString();
+		}
+		else if (xml.isCharacters() && !xml.isWhitespace())
+		{
+			if(currentTag == "name")
+			{
+				currentBunny.name = xml.text().toString();
+			}
+			else if(currentTag == "macaddress")
+			{
+				currentBunny.ID = xml.text().toString().toAscii();
+			}
+			else if(currentTag == "server")
+			{
+				currentBunny.server = xml.text().toString();
+			}
+		}
+		if(xml.isEndElement() && currentTag == "bunny")
+		{
+			whois.append(currentBunny);
+		}
+	}
+
+	return whois;
+}
 /*******/
 /* API */
 /*******/
@@ -27,6 +123,8 @@ void PluginAnnuaire::InitApiCalls()
 {
 	DECLARE_PLUGIN_API_CALL("setURL(url)", PluginAnnuaire, Api_setURL);
 	DECLARE_PLUGIN_API_CALL("getURL()", PluginAnnuaire, Api_getURL);
+	DECLARE_PLUGIN_API_CALL("searchbunnybymac(mac)", PluginAnnuaire, Api_SearchBunnyByMac);
+	DECLARE_PLUGIN_API_CALL("searchbunnybyname(name)", PluginAnnuaire, Api_SearchBunnyByName);
 }
 
 PLUGIN_API_CALL(PluginAnnuaire::Api_setURL)
@@ -48,4 +146,38 @@ PLUGIN_API_CALL(PluginAnnuaire::Api_getURL)
 		return new ApiManager::ApiError("Access denied");
 
 	return new ApiManager::ApiString(QString("%1").arg(GetSettings("global/URL", "").toString()));
+}
+
+PLUGIN_API_CALL(PluginAnnuaire::Api_SearchBunnyByMac)
+{
+	Q_UNUSED(account);
+
+	QList<BunnyInfos> whois = SearchBunnyByMac(hRequest.GetArg("mac").toAscii());
+	QString xml = "";
+	foreach(BunnyInfos b, whois)
+	{
+		xml += "<bunny>";
+		xml += "<name>" + b.name + "</name>";
+		xml += "<ID>" + b.ID + "</ID>";
+		xml += "<server>" + b.server + "</server>";
+		xml += "</bunny>\n";
+	}
+	return new ApiManager::ApiXml(xml);
+}
+
+PLUGIN_API_CALL(PluginAnnuaire::Api_SearchBunnyByName)
+{
+	Q_UNUSED(account);
+
+	QList<BunnyInfos> whois = SearchBunnyByName(hRequest.GetArg("name"));
+	QString xml = "";
+	foreach(BunnyInfos b, whois)
+	{
+		xml += "<bunny>";
+		xml += "<name>" + b.name + "</name>";
+		xml += "<ID>" + b.ID + "</ID>";
+		xml += "<server>" + b.server + "</server>";
+		xml += "</bunny>\n";
+	}
+	return new ApiManager::ApiXml(xml);
 }
--- server/plugins/system/annuaire/plugin_annuaire.h
+++ server/plugins/system/annuaire/plugin_annuaire.h
@@ -2,6 +2,13 @@
 #define _PLUGINANNUAIRE_H_
 
 #include "plugininterface.h"
+#include "bunny.h"
+
+typedef struct {
+	QString name;
+	QByteArray ID;
+	QString server;
+} BunnyInfos;
 
 class PluginAnnuaire : public PluginInterface
 {
@@ -13,10 +20,14 @@ public:
 	virtual ~PluginAnnuaire();
 
 	void OnBunnyConnect(Bunny *);
+	QList<BunnyInfos> SearchBunnyByName(QString name);
+	QList<BunnyInfos> SearchBunnyByMac(QByteArray ID);
 
 	void InitApiCalls();
 	PLUGIN_API_CALL(Api_setURL);
 	PLUGIN_API_CALL(Api_getURL);
+	PLUGIN_API_CALL(Api_SearchBunnyByName);
+	PLUGIN_API_CALL(Api_SearchBunnyByMac);
 
 };
 
