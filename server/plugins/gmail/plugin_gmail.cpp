a0f200ffac2a1506220ee2ea39cbe6d137249074
Alkorin <alkorin@openjabnab.org>
2015-09-27 17:31:05 +0200

    Re-indent gmail/plugin_gmail.cpp

--- server/plugins/gmail/plugin_gmail.cpp
+++ server/plugins/gmail/plugin_gmail.cpp
@@ -18,82 +18,89 @@ PluginGmail::PluginGmail():PluginInterface("gmail", "Gmail Configuration",BunnyP
 
 PluginGmail::~PluginGmail()
 {
-Cron::UnregisterAll(this);
+	Cron::UnregisterAll(this);
 }
 
 void PluginGmail::OnBunnyConnect(Bunny * b)
 {
-int frequency = b->GetPluginSetting(GetName(), "Frequency", 0).toInt();
-if(frequency < 1)
-frequency = 1;
-if(frequency > 500)
-frequency = 500;
-Cron::Register(this, frequency, 0, 0, b, QVariant( (int) NULL) );
+	int frequency = b->GetPluginSetting(GetName(), "Frequency", 0).toInt();
+	if(frequency < 1)
+	{
+		frequency = 1;
+	}
+	else if(frequency > 500)
+	{
+		frequency = 500;
+	}
+
+	Cron::Register(this, frequency, 0, 0, b, QVariant( (int) NULL) );
 }
 
 void PluginGmail::OnBunnyDisconnect(Bunny * b)
 {
-Cron::UnregisterAllForBunny(this, b);
-
+	Cron::UnregisterAllForBunny(this, b);
 }
 
 void PluginGmail::OnCron(Bunny * b, QVariant)
 {
-QMapIterator<Bunny *, QString> i(bunnyList);
-QString email = b->GetPluginSetting(GetName(), QString("Email"), QString()).toString();
-QString password = b->GetPluginSetting(GetName(), QString("Password"), QString()).toString();
+	QMapIterator<Bunny *, QString> i(bunnyList);
+	QString email = b->GetPluginSetting(GetName(), QString("Email"), QString()).toString();
+	QString password = b->GetPluginSetting(GetName(), QString("Password"), QString()).toString();
 
+	connect(&http, SIGNAL(readyRead(QHttpResponseHeader)),this, SLOT(readData(QHttpResponseHeader)));
+	xml.clear();
 
-    connect(&http, SIGNAL(readyRead(QHttpResponseHeader)),this, SLOT(readData(QHttpResponseHeader)));
-    xml.clear();
+	if((email != "") && (password != ""))
+	{
+		http.setHost("mail.google.com", QHttp::ConnectionModeHttps);
+		http.setUser(email, password);
+		http.setProperty("BunnyID", b->GetID());
+		http.get("/mail/feed/atom");
+	}
 
-    if((email != "") && (password != ""))
-{
-    http.setHost("mail.google.com", QHttp::ConnectionModeHttps);
-    http.setUser(email, password);
-    http.setProperty("BunnyID", b->GetID());
-    http.get("/mail/feed/atom");
-}
-
-return;
+	return;
 }
 
 
 void PluginGmail::readData(const QHttpResponseHeader &resp)
 {
-Bunny * bunny = BunnyManager::GetBunny(this, http.property("BunnyID").toByteArray());
+	Bunny * bunny = BunnyManager::GetBunny(this, http.property("BunnyID").toByteArray());
 
-    if (resp.statusCode() != 200)
-    {
-http.abort();
-LogWarning("Connection error");
-    }
-    else {
-xml.addData(http.readAll());
+	if (resp.statusCode() != 200)
+	{
+		http.abort();
+		LogWarning("Connection error");
+	}
+	else
+	{
+		xml.addData(http.readAll());
 
-while (!xml.atEnd()) {
-xml.readNext();
-if (xml.isStartElement()) {
-currentTag = xml.name().toString();
-} else if (xml.isCharacters() && !xml.isWhitespace()) {
-if (currentTag == "fullcount")
-{
-bool ok;
-emailsCount = xml.text().toString().toInt(&ok);
-if (emailsCount > 0)
-{
-bunny->SendPacket(AmbientPacket(AmbientPacket::Service_Nose, 1));
+		while (!xml.atEnd()) 
+		{
+			xml.readNext();
+			if (xml.isStartElement())
+			{
+				currentTag = xml.name().toString();
+			}
+			else if (xml.isCharacters() && !xml.isWhitespace())
+			{
+				if (currentTag == "fullcount")
+				{
+					bool ok;
+					emailsCount = xml.text().toString().toInt(&ok);
+					if (emailsCount > 0)
+					{
+						bunny->SendPacket(AmbientPacket(AmbientPacket::Service_Nose, 1));
 
-}
-else
-{
-bunny->SendPacket(AmbientPacket(AmbientPacket::Service_Nose, 0));
-
-}
-}
-}
-    }
-}
+					}
+					else
+					{
+						bunny->SendPacket(AmbientPacket(AmbientPacket::Service_Nose, 0));
+					}
+				}
+			}
+    		}
+	}
 }
 
 
@@ -103,40 +110,45 @@ bunny->SendPacket(AmbientPacket(AmbientPacket::Service_Nose, 0));
 
 void PluginGmail::InitApiCalls()
 {
-DECLARE_PLUGIN_BUNNY_API_CALL("setConfig()", PluginGmail, Api_SetConfig);
-DECLARE_PLUGIN_BUNNY_API_CALL("getConfig()", PluginGmail, Api_GetConfig);
+	DECLARE_PLUGIN_BUNNY_API_CALL("setConfig(Frequency,Email,Password)", PluginGmail, Api_SetConfig);
+	DECLARE_PLUGIN_BUNNY_API_CALL("getConfig()", PluginGmail, Api_GetConfig);
 }
 
 
 
 PLUGIN_BUNNY_API_CALL(PluginGmail::Api_SetConfig)
 {
-Q_UNUSED(account);
+	Q_UNUSED(account);
 
+	if(!hRequest.HasArg("Frequency"))
+	{
+		return new ApiManager::ApiError(QString("Missing argument 'Frequency' for plugin Gmail"));
+	}
+	if(!hRequest.HasArg("Email"))
+	{
+		return new ApiManager::ApiError(QString("Missing argument 'Email' for plugin Gmail"));
+	}
+	if(!hRequest.HasArg("Password"))
+	{
+		return new ApiManager::ApiError(QString("Missing argument 'Password' for plugin Gmail"));
+	}
 
-if(!hRequest.HasArg("Frequency"))
-return new ApiManager::ApiError(QString("Missing argument 'Frequency' for plugin Gmail"));
-if(!hRequest.HasArg("Email"))
-return new ApiManager::ApiError(QString("Missing argument 'Email' for plugin Gmail"));
-if(!hRequest.HasArg("Password"))
-return new ApiManager::ApiError(QString("Missing argument 'Password' for plugin Gmail"));
+	bunny->SetPluginSetting(GetName(), "Frequency", hRequest.GetArg("Frequency"));
+	bunny->SetPluginSetting(GetName(), "Email", hRequest.GetArg("Email"));
+	bunny->SetPluginSetting(GetName(), "Password", hRequest.GetArg("Password"));
 
-bunny->SetPluginSetting(GetName(), "Frequency", hRequest.GetArg("Frequency"));
-bunny->SetPluginSetting(GetName(), "Email", hRequest.GetArg("Email"));
-bunny->SetPluginSetting(GetName(), "Password", hRequest.GetArg("Password"));
-
-return new ApiManager::ApiOk(QString("Account '%1' will be verified every '%2' minute(s) for bunny '%3'").arg(hRequest.GetArg("Email"),hRequest.GetArg("Frequency"), QString(bunny->GetID())));
+	return new ApiManager::ApiOk(QString("Account '%1' will be verified every '%2' minute(s) for bunny '%3'").arg(hRequest.GetArg("Email"),hRequest.GetArg("Frequency"), QString(bunny->GetID())));
 }
 
 PLUGIN_BUNNY_API_CALL(PluginGmail::Api_GetConfig)
 {
-    Q_UNUSED(account);
-Q_UNUSED(hRequest);
+	Q_UNUSED(account);
+	Q_UNUSED(hRequest);
 
-QList<QString> list;
-list.append(bunny->GetPluginSetting(GetName(), "Frequency", QString()).toString());
-list.append(bunny->GetPluginSetting(GetName(), "Email", QString()).toString());
-list.append(bunny->GetPluginSetting(GetName(), "Password", QString()).toString());
+	QList<QString> list;
+	list.append(bunny->GetPluginSetting(GetName(), "Frequency", QString()).toString());
+	list.append(bunny->GetPluginSetting(GetName(), "Email", QString()).toString());
+	list.append(bunny->GetPluginSetting(GetName(), "Password", QString()).toString());
 
-return new ApiManager::ApiList(list);
+	return new ApiManager::ApiList(list);
 }
